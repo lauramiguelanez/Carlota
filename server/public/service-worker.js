c145bf787ab6e1b01bcabdd7cf7b9327dd779720
Laura Migueláñez <laura.miguelanez@gmail.com>
2018-12-16 19:00:45 +0100

    Deploy test2

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33a82ce129f0ee3401f17992f55b1b76.js"
+  "/precache-manifest.d6097b9f104583703c62c96e5f7acc30.js"
 );
 
 workbox.clientsClaim();
