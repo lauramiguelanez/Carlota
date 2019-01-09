91c014a73c7a1e75f3c4a0135abbc865aee3ab0d
Laura Migueláñez <laura.miguelanez@gmail.com>
2019-01-09 22:56:32 +0100

    Projects fix build

---
+++ server/public/precache-manifest.725a98f1eb67cad5d288f3719446818b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcaa45b3f41c58c8e4f5",
+    "url": "/static/css/main.7c0260ec.chunk.css"
+  },
+  {
+    "revision": "fcaa45b3f41c58c8e4f5",
+    "url": "/static/js/main.fcaa45b3.chunk.js"
+  },
+  {
+    "revision": "55af87b3a163da6dd4b9",
+    "url": "/static/js/1.55af87b3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12bc7db3070ea7cbcbb7220806763219",
+    "url": "/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57bc6bb80b6996e4d4017b20a10670ee.js"
+  "/precache-manifest.725a98f1eb67cad5d288f3719446818b.js"
 );
 
 workbox.clientsClaim();
